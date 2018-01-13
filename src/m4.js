c848a480fb0de31d6fbeeffda7d123f577b1f245
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-01-13 21:59:59 +0530

    some fixes

--- src/m4.js
+++ src/m4.js
@@ -6,7 +6,7 @@ class M4 extends Component{
     render(){
         return(
             <div className="img-fluid max-width:100%" >
-                <img style={{margin:4}} className="img-fluid max-width:1250xp " alt="need to be filled , right now empty " src="http://via.placeholder.com/1200x400"/>
+                <img style={{marginTop:20}} className="img-fluid max-width:1250xp " alt="need to be filled , right now empty " src="http://via.placeholder.com/1250x400"/>
             </div> 
         );
     }
