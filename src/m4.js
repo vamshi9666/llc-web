c9ffc52865c3a9ca47629a65beca8b109eef6ac0
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-01-13 15:41:33 +0530

    m4

--- src/m4.js
+++ src/m4.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import img from 'react-bootstrap';
+import './m4.css'
 
 class M4 extends Component{
     render(){
         return(
-            <div className="Img-con" >
-                <img className="img-fluid" alt="need to be filled , right now empty " src="http://via.placeholder.com/1100x400"/>
-            </div>   
+            <div className="img-fluid max-width:100%" >
+                <img style={{margin:4}} className="img-fluid max-width:1250xp " alt="need to be filled , right now empty " src="http://via.placeholder.com/1200x400"/>
+            </div> 
         );
     }
 }
