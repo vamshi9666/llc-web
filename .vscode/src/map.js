e94d5350fe2ef6d2bef879b16be6ed02440bc490
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-01-14 10:55:51 +0530

    added map

---
+++ .vscode/src/map.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import img from 'react-bootstrap';
+ 
+class Map extends Component{
+
+    render(){
+        return(
+            
+        );
+    }
+}
+
+
+export default Map;
