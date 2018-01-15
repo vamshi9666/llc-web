7ea17a2b57a42532d3025ea7be104340d6a0add2
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-01-15 13:56:25 +0530

    ..

--- src/m3.js
+++ src/m3.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import img from 'react-bootstrap';
+import ScroolArea from 'react-scrollbar';
+
+
 
 class M3 extends Component {
     render(){
         return(
-          
+          <div className="x-scrooler">
+
+       
+      
             <div className="row">
               <div className="col-sm-4">
                 <div className="card">
@@ -22,8 +28,10 @@ class M3 extends Component {
                  <img src="http://via.placeholder.com/200x200"/>
                 </div>
               </div>
-            </div>  
+              
+            </div>   
              
+        </div>     
 
 
             
