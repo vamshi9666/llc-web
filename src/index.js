3bc59180085510f2cd2d39be8e9310e6d61953cf
vamshi9666 <vamshi123nimmakuri123@gmail.com>
2018-07-22 20:23:27 +0530

    spaces

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import M2 from'./m2';
-import M3 from'./m3';
+import M2 from './m2';
+import M3 from './m3';
 import M4 from './m4';
 import App from './App';
-import Map from'./map';
+import Map from './map';
 import People from './people';
 
 import registerServiceWorker from './registerServiceWorker';
@@ -20,14 +20,14 @@ class APP extends Component{
                  <M4 />
                  <Map  />
                  <People />
-                 
-            </div> 
+
+            </div>
 
         );
     }
 }
 
 ReactDOM.render(
-  <APP/> 
+  <APP/>
 , document.getElementById('root'));
 registerServiceWorker();
